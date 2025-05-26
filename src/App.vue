c7204fed89cf11e2c8bbb7080f512d5012aeb6a4
Luis Calabro <Luiskpro@gmail.com>
2025-05-25 21:38:58 -0300

    con tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,12 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
 
-
-<script setup>
 </script>
 
 <template>
-  <div class="min-h-screen bg-gray-100 flex items-center justify-center">
-    <div class="bg-blue-500 text-white p-8 rounded-xl shadow-lg hover:bg-blue-600 transition-colors text-2xl font-bold">
-      Hola Tailwind!
-    </div>
-  </div>
+<div> <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1></div>
 </template>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
